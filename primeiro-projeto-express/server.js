7c506d6f36e604d670b910256c34b1f7147020fc
Juliana Karla Camargo da Silva <juliana.silva@2rpnet.com>
2026-09-23 22:43:00 -0300

    adição do cors

--- primeiro-projeto-express/server.js
+++ primeiro-projeto-express/server.js
@@ -17,3 +17,12 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
+
+module.exports = app;
+
+const cors = require("cors");
+
+// Permite que a Vercel acesse a API do Render
+app.use(cors({
+    origin: "https://projeto-api-express-aab3.onrender.com"
+}));
